40bad522259c01f7e9d4bf55fc132740a8d00c36
prashanth.balakrishnan <prashanth.balakrishnan@amal.com>
2024-02-23 21:38:03 +0400

    Fixing connection string

--- paywallet/settings.py
+++ paywallet/settings.py
@@ -115,11 +115,11 @@ if os.environ.get("ENVIRONMENT") in ["PRODUCTION", "STAGING"]:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("DB_NAME"),
-            "USER": os.environ.get("DB_USER"),
-            "PASSWORD": os.environ.get("DB_PASSWORD"),
-            "HOST": os.environ.get("DB_HOST"),
-            "PORT": os.environ.get("DB_PORT"),
+            "NAME": os.environ.get("RDS_DB_NAME"),
+            "USER": os.environ.get("RDS_USERNAME"),
+            "PASSWORD": os.environ.get("RDS_PASSWORD"),
+            "HOST": os.environ.get("RDS_HOSTNAME"),
+            "PORT": os.environ.get("RDS_PORT"),
         }
     }
 
