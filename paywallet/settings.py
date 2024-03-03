fba1069bdeb3c91bd91b0ca5179e246b30371459
prashanth.balakrishnan <prashanth.balakrishnan@amal.com>
2024-03-03 10:54:13 +0400

    Exception is local environment. Making staging as default

--- paywallet/settings.py
+++ paywallet/settings.py
@@ -29,12 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ug+p47x#8&h*ir3xjh%+s9z(q-*=7so7nk9lnlo7r8m2o)j2w1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-]
 
 
 # Application definition
@@ -99,29 +93,31 @@ WSGI_APPLICATION = "paywallet.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-default_db = {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": BASE_DIR / "db.sqlite3",
-}
+DEBUG = False
+ALLOWED_HOSTS = [
+    "generations-staging.eu-west-1.elasticbeanstalk.com"]
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("RDS_DB_NAME"),
+        "USER": os.environ.get("RDS_USERNAME"),
+        "PASSWORD": os.environ.get("RDS_PASSWORD"),
+        "HOST": os.environ.get("RDS_HOSTNAME"),
+        "PORT": os.environ.get("RDS_PORT"),
     }
 }
-if os.environ.get("ENVIRONMENT") in ["PRODUCTION", "STAGING"]:
-    DEBUG = False
+
+if os.environ.get("ENVIRONMENT") in ["local"]:
+    DEBUG = True
+
     ALLOWED_HOSTS = [
-        "generations-staging.eu-west-1.elasticbeanstalk.com",
-        "paywallet-test.eba-gpcbmfsa.eu-west-1.elasticbeanstalk.com"]
+        "127.0.0.1",
+        "localhost",
+    ]
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("RDS_DB_NAME"),
-            "USER": os.environ.get("RDS_USERNAME"),
-            "PASSWORD": os.environ.get("RDS_PASSWORD"),
-            "HOST": os.environ.get("RDS_HOSTNAME"),
-            "PORT": os.environ.get("RDS_PORT"),
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 
