0b8d66d9adcd72d5d9b9d840bbe9045891b9a2e3
prashanth.balakrishnan <prashanth.balakrishnan@amal.com>
2024-03-10 06:50:19 +0400

    Allow image upload to allergies

--- product/models/allergy.py
+++ product/models/allergy.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from paywallet.storage_backends import PublicMediaStorage
+
 
 class Allergy(models.Model):
     # Add a name field with localized string
     name = models.CharField(max_length=100, verbose_name=_("Name"))
+    image = models.ImageField(storage=PublicMediaStorage(),
+                              upload_to='allergies/',  blank=True, verbose_name=_("Image"))
 
     def __str__(self):
         return self.name
